8c547002e29bab768b8d23f7a435deadecd10ae2
Federico Ghedina <fedeghe@gmail.com>
2017-10-29 22:10:25 +0100

    EXE almost works in nested json

--- src/starter.js
+++ src/starter.js
@@ -9,69 +9,67 @@ var Malta = require("./malta"),
 
 module.exports = Malta;
 
+M(args, len);
 
-if (len == 0) {
+function M(_args, _len) {
+	// no params -> print help and exit
+	if (_len == 0) {
 
-	BIN_MODE && Malta.log_help();
+		BIN_MODE && Malta.log_help();
 
-} else if (len == 1){
+	// just one param is given -> is a build json file
+	//
+	} else if (_len == 1){
 
-	Malta.outVersion();
-	var p = path.resolve(execPath, args[0]),
-		runs = Malta.getRunsFromPath(p),
-		tpl,
-		tmp,
-		nest;
+		Malta.outVersion();
+		var p = path.resolve(execPath, _args[0]),
+			runs = Malta.getRunsFromPath(p),
+			tpl,
+			tmp,
+			nest;
 
-	!runs && Malta.badargs(p);
+		!runs && Malta.badargs(p);
 
-	if ('EXE' in runs) {
-		(function (commands) {
-			Malta.log_info('Executing command in EXE section')
-			var c,
-				i = 0
-				clen = commands.length;
-			clen ? (function start() {
-				if (i < clen - 1) {
-					Malta.execute(commands[i].split(/\s/), function (){++i; start();});
+		if ('EXE' in runs) {
+			(function (commands) {
+				Malta.log_info('Executing command in EXE section for ' + _args[0])
+				var c,
+					i = 0
+					clen = commands.length;
+				clen ? (function start() {
+					if (i < clen - 1) {
+						Malta.execute(commands[i].split(/\s/), function (){++i; start();});
+					} else {
+						Malta.execute(commands[i].split(/\s/), function (){delete runs.EXE; go(runs);});
+					}
+					if (i == clen - 1){
+						console.log('...done!');
+					}
+				})(i) : go(runs);
+			})(runs.EXE);
+		} else {
+			go(runs);
+		}
+
+		function go(_runs){
+			var i, tmpArr;
+			for (tpl in _runs) {
+
+				//check if is inclusion {whatever.json : true}
+				// 
+				if (tpl.match(/\.json$/) && _runs[tpl] === true) {
+					M([tpl], 1);
 				} else {
-					Malta.execute(commands[i].split(/\s/), function (){delete runs.EXE; go(runs);});
+					//skip if key begins with !
+					if (tpl.match(/^\!/)) continue;
+					functions.multi(tpl, _runs[tpl]);
 				}
-				if (i == clen - 1){
-					console.log('...done!');
-				}
-			})(i) : go(runs);
-		})(runs.EXE);
-	} else {
-		go(runs);
-	}
-
-	function go(_runs){
-
-		for (tpl in _runs) {
-
-			//check if is inclusion {whatever.json : true}
-			// 
-			if (tpl.match(/\.json$/) && _runs[tpl] === true) {
-
-				p = path.resolve(execPath, tpl),
-				nest = fs.existsSync(p) ? require(p) :  false;
-				for (tmp in nest) {
-					if (tmp.match(/^\!/)) continue;
-					functions.multi(tmp, nest[tmp]);
-				}
-				
-			} else {
-				//skip if key begins with !
-				if (tpl.match(/^\!/)) continue;
-				functions.multi(tpl, _runs[tpl]);
 			}
 		}
+
+	// single build
+	} else {
+		Malta.outVersion();
+		Malta.get().check(_args).start();
 	}
-
-} else {
-
-	Malta.outVersion();
-	Malta.get().check(args).start();
-
 }
