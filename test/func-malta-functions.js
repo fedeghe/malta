edc71ce8e9e36a87ffe2d4b5331d440df4cb74f8
fedeghe <fedeghe@gmail.com>
2019-02-16 23:00:11 +0100

    check -clean

---
+++ test/func-malta-functions.js
@@ -0,0 +1,18 @@
+var assert = require('assert'),
+	fs = require('fs'),
+	path = require('path'),
+	functions = require('../src/functions');
+
+describe('check subCommands', function () {
+	var trgFolder = path.resolve(path.dirname(__filename) + '/fs');
+	it('-clean works', function (done) {
+		// first create a *.buildNum.json
+		fs.writeFile(trgFolder + '/aaa.buildNum.json', '', function () {
+			if (fs.existsSync(trgFolder + '/aaa.buildNum.json')) {
+				var res = functions.subCommand('-clean');
+				assert.equal(res, true);
+			}
+			done();
+		});
+	});
+});
