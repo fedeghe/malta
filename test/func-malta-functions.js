d92f5f5bc3298a710553ab5e7a9648c450db0c3a
fedeghe <fedeghe@gmail.com>
2019-03-05 00:49:16 +0100

    proceed and multi almost done

--- test/func-malta-functions.js
+++ test/func-malta-functions.js
@@ -3,11 +3,11 @@ const assert = require('assert'),
 	path = require('path'),
 	functions = require('src/functions');
 
-describe('check subCommands', function () {
+describe('check subCommands', () => {
 	var trgFolder = path.resolve(path.dirname(__filename) + '/fs');
 	it('-clean works', function (done) {
 		// first create a *.buildNum.json
-		fs.writeFile(trgFolder + '/aaa.buildNum.json', '', function () {
+		fs.writeFile(trgFolder + '/aaa.buildNum.json', '', () => {
 			if (fs.existsSync(trgFolder + '/aaa.buildNum.json')) {
 				var res = functions.subCommand('-clean');
 				assert.equal(res, true);
@@ -15,7 +15,44 @@ describe('check subCommands', function () {
 			done();
 		});
 	});
-	it('-clean is the only command', function (done) {
+	it('-clean is the only command', done => {
 		!functions.subCommand('-unclean') && done();
 	});
+});
+
+describe('proceed', () => {
+	it('should create more destinations from a single file', (done) => {
+		const dests = [
+				'test/fs/functions/proceed/dest1',
+				'test/fs/functions/proceed/dest2',
+				'test/fs/functions/proceed/dest3'
+			],
+			maltas = functions.proceed('#test/fs/functions/proceed/tpl.js', dests);
+		let num = dests.length;
+			
+		maltas.forEach(m => m.then(function() { // need this here, leave the es5
+			fs.unlink(this.outName, () => {
+				num--;
+				if (num === 0) done();
+			});
+		}));
+	})
+});
+
+describe('multi', () => {
+	it('should use wildcard as source tpl', (done) => {
+		const dest = 'test/fs/functions/multi/dest',
+			maltas = functions.multi('#test/fs/functions/multi/*.js', dest);
+
+		let num = Object.keys(maltas).length;
+
+		for (var m in maltas) {
+			maltas[m].then(function () { // need this here, leave the es5
+				fs.unlink(this.outName, () => {
+					num--;
+					if (num === 0) done();
+				});
+			});
+		}
+	}).timeout(10000);
 });
