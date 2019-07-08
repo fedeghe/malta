0c209e6169654e3ef6ae4dd5a07a41d3e5eadce8
fedeghe <fedeghe@gmail.com>
2019-07-08 23:16:53 +0200

    test expressions new ph

---
+++ test/fs/build/expression_ph.js
@@ -0,0 +1,6 @@
+var a = true * 2 + false * 3 + true * 5,
+	b = true * 2 + false * 3 + true * 5 ,
+    c = "just a string",
+    e = 10,
+    d = Math.pow(e, 3)* 4 / 3 * Math.PI,
+    sphereVolume = d;
