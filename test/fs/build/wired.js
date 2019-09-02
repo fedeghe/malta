a9f2d0917b10dfa1c44b9e9bf588cd8bbd216c38
fedeghe <fedeghe@gmail.com>
2019-09-02 23:38:43 +0200

    up

--- test/fs/build/wired.js
+++ test/fs/build/wired.js
@@ -1,8 +1,8 @@
-var time = '21:9:43',
-    date = '15/7/2019'
+var time = '23:36:2',
+    date = '2/9/2019'
     year = '2019',
     filesnum = '2',
-    version = '4.0.3',
+    version = '4.0.4',
     buildnumber = '1',
     file = 'wired.js',
     line = '8';
