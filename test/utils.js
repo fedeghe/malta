a8a531d51ff79e25936484ecb024736877d4f819
fedeghe <fedeghe@gmail.com>
2020-03-07 23:36:56 +0100

    domefunc to cleanup

---
+++ test/utils.js
@@ -0,0 +1,24 @@
+const child_process = require('child_process'),
+    assert = require('assert'),
+    malta = require('../src/index')
+
+const doneFunc = folder => done => {
+    try {
+        var ls = child_process.spawn('node', ['src/bin.js', `${folder}/clean.json`]);
+        ls.on('exit', function (code) {
+            assert.equal(code, 0);
+            assert.equal(malta.executeCheck, code); // 0
+            //check the files?...not yet
+            done();
+        });
+        ls.stderr.on('data', function(err) {
+            assert.ok(false)
+        });
+    } catch (err) {
+        throw err;
+    }
+}
+
+module.exports = {
+    doneFunc
+}
